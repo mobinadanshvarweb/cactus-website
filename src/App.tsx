bc9629c310a5ac15a7422a0b4c44f1b7f39bdce5
mobinadanshvarweb <mobinadaneshvardeveloper@gmail.com>
2025-05-19 15:55:05 +0330

    about us

--- src/App.tsx
+++ src/App.tsx
@@ -65,7 +65,33 @@ const App = () => {
       {/* about us */}
       <section className="w-full min-h-screen flex flex-col justify-center items-center">
         <Title text="درباره ما" />
-        <div className="flex-1 bg-gray-200 w-full"></div>
+        <div className="flex-1 w-full">
+          <div className="bg-[url(/imgs/about.jpg)] bg-cover bg-center bg-fixed h-screen">
+            <div className="w-full bg-black/50 h-full flex items-center justify-center p-10 lg:justify-start lg:p-16">
+              <div className="w-full md:w-1/2 h-full bg-[#bfc0b9] custom-clip flex justify-center items-center">
+                <div className="w-[80%] h-[90%]  text-justify overflow-y-scroll custom-scroll">
+                  لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و
+                  با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و
+                  مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
+                  تکنولوژی مورد نیاز، و کاربردهای متنوع با هدف بهبود ابزارهای
+                  کاربردی می باشد، کتابهای زیادی در شصت و سه درصد گذشته حال و
+                  آینده، شناخت فراوان جامعه و متخصصان را می طلبد، تا با نرم
+                  افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص
+                  طراحان خلاقی، و فرهنگ پیشرو در زبان فارسی ایجاد کرد، در این
+                  صورت می توان امید داشت که تمام و دشواری موجود در ارائه
+                  راهکارها، و شرایط سخت تایپ به پایان رسد و زمان مورد نیاز شامل
+                  حروفچینی دستاوردهای اصلی، و جوابگوی سوالات پیوسته اهل دنیای
+                  موجود طراحی اساسا مورد استفاده قرار گیرد.لورم ایپسوم متن
+                  ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و با استفاده از
+                  طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون
+                  و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
+                  و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، برای
+                  طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشگیرد.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </section>
       {/* products */}
       <section className="w-full min-h-screen flex flex-col justify-center items-center">
