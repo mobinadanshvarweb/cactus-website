5d3cca94d016107e66d23870eda091b80cb273cc
mobinadanshvarweb <mobinadaneshvardeveloper@gmail.com>
2025-05-21 13:04:55 +0330

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ const App = () => {
           <div className="bg-[url(/imgs/about.jpg)] bg-cover bg-center bg-fixed h-screen">
             <div className="w-full bg-black/50 h-full flex items-center justify-center p-10 lg:justify-start lg:p-16">
               <div className="w-full md:w-1/2 h-full bg-[#bfc0b9] custom-clip flex justify-center items-center">
-                <div className="w-[80%] h-[90%]  text-justify overflow-y-scroll custom-scroll">
+                <div className="w-[80%] h-[90%]  text-justify overflow-y-scroll custom-scroll text-[#2c8039]">
                   لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ، و
                   با استفاده از طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و
                   مجله در ستون و سطرآنچنان که لازم است، و برای شرایط فعلی
@@ -86,7 +86,7 @@ const App = () => {
                   طراحان گرافیک است، چاپگرها و متون بلکه روزنامه و مجله در ستون
                   و سطرآنچنان که لازم است، و برای شرایط فعلی تکنولوژی مورد نیاز،
                   و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد، برای
-                  طراحان رایانه ای علی الخصوص طراحان خلاقی، و فرهنگ پیشگیرد.
+                  نگ پیشگیرد.
                 </div>
               </div>
             </div>
